ee8eb860b9ed3f5aaf2aef4e6aeebcb18fc9ad57
Kiyotaka <hiroya741@gmail.com>
2025-09-26 01:52:23 +0000

    GAS

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let isAutoUpdating = false;
 
 // GAS接続設定
 // ★★★ ここにデプロイしたGASの「テスト実行」URLを入れるんやで！ ★★★
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwIpI_-rQs44T-h4mhlRBlXJOfP0fVpeqFwVu1IkdW4QXo1cu8wxrzpVaecSD2DIaeSfw/exec'; 
+const GAS_URL = 'https://script.googleusercontent.com/macros/s/AKfycbz_9egCx1hsSKI76m4co16QoCsOr7HhaXVeakrGYzLwOO5u4m-9iK8c5WigGtuL1Q0rmg/exec'; 
 const CACHE_KEY = 'chordAppCache';
 const CACHE_TTL = 3600000; // キャッシュ有効期間: 1時間 (ミリ秒)
 
